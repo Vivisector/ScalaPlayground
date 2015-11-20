4b05cfaa92723927905fc5c3a751467aeaea922e
Igor Ivaniuk <i.ivanyuk@gmail.com>
2015-11-21 00:21:03 +0100

    Chapter 1 - playground

--- src/main/scala/com/igori/App.scala
+++ src/main/scala/com/igori/App.scala
@@ -1,9 +1,24 @@
 package com.igori
 
+import scala.collection.immutable.StringOps
+import scala.util.Random
+import scala.math.BigInt._
+
 /**
- * Hello world!
- *
- */
+  * Hello world!
+  *
+  */
 object MyApp extends App {
-  println( "Hello World!" )
+  var res: BigInt = 2;
+  println(res.pow(1024))
+
+  println("Yuriy"*3)
+
+  println(probablePrime(100,Random))
+
+  println(probablePrime(100, Random).toString(36))
+
+  var s = new StringOps("test string")
+
+
 }
