1f02438f63dc57af0ed7eefa2b90e91282f55984
unknown <igori@ND040.zooplus.local>
2015-11-25 15:07:22 +0100

    Chapter 2 exercises started

--- src/main/scala/com/igori/App.scala
+++ src/main/scala/com/igori/App.scala
@@ -1,24 +1,42 @@
 package com.igori
 
 import scala.collection.immutable.StringOps
-import scala.util.Random
 import scala.math.BigInt._
+import scala.util.Random
 
 /**
   * Hello world!
   *
   */
-object MyApp extends App {
+object Chapter1 extends App {
   var res: BigInt = 2;
   println(res.pow(1024))
 
-  println("Yuriy"*3)
+  println("Yuriy" * 3)
 
-  println(probablePrime(100,Random))
+  println(probablePrime(100, Random))
 
   println(probablePrime(100, Random).toString(36))
 
   var s = new StringOps("test string")
+}
 
+/**
+  * Chapter 2
+  */
+object Chapter2 extends App {
+
+  def signum(number: Int): Int = {
+    if (number == 0)
+      0
+    else if (number > 0)
+      1
+    else
+      -1
+  }
+
+  println("Signum: 5 = " + signum(5));
+  println("Signum: 0 = " + signum(0));
+  println("Signum: -1 = " + signum(-1));
 
 }
